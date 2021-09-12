364b4c248d2656587e8e4358e1c646f4fac8e7d0
wayson <wayson.1104@gmail.com>
2021-09-12 13:00:52 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'arbitrium-server',
+    name = 'arbitrium-site',
     version = '0.1.0',
     packages = find_packages(),
     entry_points = {
